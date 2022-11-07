e4603269dea845cfbaa09555cb7b4332d1845531
Meli56 <bodiou.amelie@gmail.com>
2022-11-07 15:17:23 +0100

    mise en place backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -2,7 +2,7 @@ import { useParams } from 'react-router-dom';
 
 function ProductScreen() {
     const params = useParams();
-    const { slug } = params;
+    const {slug} = params;
     return (
         <div>
             <h1>{slug}</h1>
