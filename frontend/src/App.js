c1a60efb8352f00a76a9c2f0e39a19b76df2adb1
Meli56 <bodiou.amelie@gmail.com>
2022-11-07 11:13:10 +0100

    premier commit mise en place frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div >
+      <header >
+        <a href="/">Samani</a>
+      </header>
+        <main>
+            List products
+        </main>
+    </div>
+  );
+}
+
+export default App;
