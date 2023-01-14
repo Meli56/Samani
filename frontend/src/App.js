596aaeed6dcf3880b7d1a2652f7a72c49522b207
Meli56 <bodiou.amelie@gmail.com>
2023-01-14 15:29:26 +0100

    ajout au panier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,11 @@ import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import Header from "./screens/Header";
-import {Container, Navbar, NavbarBrand} from "react-bootstrap";
+import {Badge, Container, Nav, Navbar, NavbarBrand} from "react-bootstrap";
 import {LinkContainer} from 'react-router-bootstrap';
 import Bandeau from "./screens/Bandeau";
+import {useContext} from "react";
+import {Store} from "./Store";
 /*
                   <Routes>
                       <Route path="/" element={<Bandeau />} />
@@ -13,7 +15,10 @@ import Bandeau from "./screens/Bandeau";
  */
 
 function App() {
-  return (
+    const {state} = useContext(Store);
+    const {cart} = state;
+
+    return (
 
       <BrowserRouter>
           <div className="d-flex flex-column site-container">
@@ -21,13 +26,18 @@ function App() {
                   <Navbar bg="dark" variant="dark">
                       <Container>
                           <LinkContainer to="/">
-                          <NavbarBrand className="nav">
-                              <Routes>
-                                  <Route path="/" element={<Header />} />
-                              </Routes>
-                              SAMANI
-                          </NavbarBrand>
+                              <Navbar.Brand>SAMANI</Navbar.Brand>
                           </LinkContainer>
+                          <Nav className="me-auto">
+                              <Link to="/cart" className="nav-link">
+                                  Cart
+                                  {cart.cartItems.length > 0 && (
+                                      <Badge pill bg="danger">
+                                          {cart.cartItems.length}
+                                      </Badge>
+                                  )}
+                              </Link>
+                          </Nav>
                       </Container>
                   </Navbar>
               </header>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,5 @@
 import { useParams } from 'react-router-dom';
-import {useEffect, useReducer} from "react";
+import {useContext, useEffect, useReducer} from "react";
 import axios from "axios";
 import {Badge, Button, Card, Col, ListGroup, ListGroupItem, Row} from "react-bootstrap";
 import Rating from "../components/Rating";
@@ -7,6 +7,7 @@ import {Helmet} from "react-helmet-async";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 import {getError} from "../utils";
+import {Store} from "../Store";
 
 
 const reducer = (state, action) => {
@@ -44,6 +45,13 @@ function ProductScreen() {
         fetchData();
     }, [slug]);
 
+    const { state, dispatch: ctxDispatch } = useContext(Store);
+    const addToCartHandler = () => {
+        ctxDispatch({
+            type: 'CART_ADD_ITEM',
+            payload: { ...product, quantity: 1 },
+        });
+    };
     return loading ? (
         <LoadingBox />
     ) : error ? (
@@ -110,7 +118,7 @@ function ProductScreen() {
                                 {product.countInStock > 0 && (
                                     <ListGroupItem>
                                         <div className="d-grid">
-                                            <Button variant="primary">
+                                            <Button onClick={addToCartHandler} variant="primary">
                                                 Add to Cart
                                             </Button>
                                         </div>
