d983a13c061dda69b05f5908ce227e1541d53594
Meli56 <bodiou.amelie@gmail.com>
2023-02-04 14:40:37 +0100

    create order screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import ShippingAddressScreen from "./screens/ShippingAddressScreen";
 import SignupScreen from "./screens/SignupScreen";
 import PaymentMethodScreen from "./screens/PaymentMethodScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
 
 /*
                   <Routes>
@@ -90,6 +91,9 @@ function App() {
                       <Route path="/signin" element={<SigninScreen />} />
                       <Route path="/signup" element={<SignupScreen />} />
                       <Route path="/placeorder" element={<PlaceOrderScreen />} />
+
+                      <Route path="/order/:id" element={<OrderScreen />} />
+
                       <Route path="/shipping" element={<ShippingAddressScreen />} />
                       <Route path="/payment" element={<PaymentMethodScreen />}></Route>
                       <Route path="/" element={<HomeScreen />} />
