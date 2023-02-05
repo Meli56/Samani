2f8299c17523f69f1bea52fa9d1c6476e5107579
Meli56 <bodiou.amelie@gmail.com>
2023-02-05 20:43:38 +0100

    searchbox et categories

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import Product from "../models/productModel.js";
 import data from "../data.js";
+import expressAsyncHandler from "express-async-handler";
 
 const productRouter = express.Router();
 
@@ -8,6 +9,19 @@ productRouter.get('/', async (req, res) => {
     const products = await Product.find();
     res.send(products);
 });
+
+
+productRouter.get(
+    '/categories',
+    expressAsyncHandler(async (req, res) => {
+        const categories = await Product.find().distinct('category');
+        res.send(categories);
+    })
+);
+
+
+
+
 productRouter.get('/slug/:slug', async (req, res) => {
     const product = await Product.findOne((x) => x.slug === req.params.slug);
     if (product) {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,24 @@
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
-import {Badge, Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {Badge, Button, Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import {LinkContainer} from 'react-router-bootstrap';
 import Bandeau from "./screens/Bandeau";
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import {Store} from "./Store";
 import CartScreen from './screens/CartScreen';
 import SigninScreen from "./screens/SigninScreen";
-import {ToastContainer} from "react-toastify";
+import {toast, ToastContainer} from "react-toastify";
 import ShippingAddressScreen from "./screens/ShippingAddressScreen";
 import SignupScreen from "./screens/SignupScreen";
 import PaymentMethodScreen from "./screens/PaymentMethodScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import OrderHistoryScreen from "./screens/OrderHistoryScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import {getError} from "./utils";
+import axios from "axios";
+import SearchBox from './components/SearchBox';
 
 /*
                   <Routes>
@@ -32,27 +36,85 @@ function App() {
         localStorage.removeItem('userInfo');
         localStorage.removeItem('shippingAddress');
         localStorage.removeItem('paymentMethod');
+        window.location.href = '/signin';
     };
 
+    const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const { data } = await axios.get(`/api/products/categories`);
+                setCategories(data);
+            } catch (err) {
+                toast.error(getError(err));
+            }
+        };
+        fetchCategories();
+    }, []);
     return (
 
       <BrowserRouter>
-          <div className="d-flex flex-column site-container">
+              <div
+                  className={
+                      sidebarIsOpen
+                          ? 'd-flex flex-column site-container active-cont'
+                          : 'd-flex flex-column site-container'
+                  }
+              >
               <ToastContainer position="bottom-center" limit={1} />
               <header >
                   <Navbar bg="dark" variant="dark" className="nav" expand="lg">
                       <Container>
+                          <Button
+                              variant="dark"
+                              onClick={() => setSidebarIsOpen(!sidebarIsOpen)}
+                          >
+                              <i className="fas fa-bars"></i>
+                          </Button>
+                          <div
+                              className={
+                                  sidebarIsOpen
+                                      ? 'active-nav side-navbar d-flex justify-content-between flex-wrap flex-column'
+                                      : 'side-navbar d-flex justify-content-between flex-wrap flex-column'
+                              }
+                          >
+                              <Nav className="flex-column text-white w-100 p-2 bg-black">
+                                  <Nav.Item>
+                                      <strong>Categories</strong>
+                                  </Nav.Item>
+                                  <Nav.Item>
+                                      <LinkContainer to="/" onClick={() => setSidebarIsOpen(false)}>
+                                          <Nav.Link>All</Nav.Link>
+                                      </LinkContainer>
+                                  </Nav.Item>
+                                  {categories.map((category) => (
+
+                                      <Nav.Item key={category}>
+                                          <LinkContainer
+                                              to="`/category=${category}`"
+                                              onClick={() => setSidebarIsOpen(false)}
+                                          >
+                                              <Nav.Link>{category}</Nav.Link>
+                                          </LinkContainer>
+
+                                      </Nav.Item>
+                                  ))}
+
+                              </Nav>
+                          </div>
                           <LinkContainer to="/">
                               <Navbar.Brand>FIHAZGIN</Navbar.Brand>
                           </LinkContainer>
-
+                          <SearchBox />
                           <Navbar.Toggle aria-controls="basic-navbar-nav" />
                           <Navbar.Collapse id="basic-navbar-nav">
                               <Nav className="me-auto  w-100  justify-content-end">
                                   <Link to="/cart" className="nav-link">
                                       Cart
                                       {cart.cartItems.length > 0 && (
-                                          <Badge pill bg="danger">
+                                          <Badge pill bg="danger" className="mx-2">
                                               {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
                                           </Badge>
                                       )}
@@ -84,6 +146,8 @@ function App() {
                       </Container>
                   </Navbar>
               </header>
+
+
               <main className="contain">
                   <Routes>
                       <Route path="/" element={<Bandeau />} />
@@ -99,6 +163,7 @@ function App() {
 
                       <Route path="/orderhistory" element={<OrderHistoryScreen />}></Route>
                       <Route path="/order/:id" element={<OrderScreen />} />
+                      <Route path="/profile" element={<ProfileScreen />} />
 
                       <Route path="/shipping" element={<ShippingAddressScreen />} />
                       <Route path="/payment" element={<PaymentMethodScreen />}></Route>
